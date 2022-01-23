54b7b0d79ed065eb08a229f5c0f0040a54ec1052
firelord69 <79193827+firelord69@users.noreply.github.com>
2022-01-23 22:08:16 +0530

    Add files via upload

---
+++ classifier.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from PIL import Image
+import PIL.ImageOps
+
+X = np.load('image.npz')['arr_0']
+y = pd.read_csv('labels.csv')['labels']
+
+print(pd.Series(y).value_counts())
+
+classes = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+nclasses = len(classes)
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state = 42,train_size=7500,test_size=2500)
+
+X_train_scaled = X_train/255
+X_test_scaled = X_test/255
+
+clf = LogisticRegression(slover='saga',multi_class='multinomial').fit(X_train_scaled,y_train)
+
+def get_predicition(image):
+    im_pil = Image.open(image)
+    imagebw = im_pil.convert('L')
+    imagebwresized = imagebw.resize((28,28), Image.ANTIALIAS)
+    pixelfilter = 20
+    minpixel = np.percentile(imagebwresized, pixelfilter)
+    imagebwresizedinvertedscaled = np.clip(imagebwresized-minpixel, 0, 255)
+    max_pixel = np.max(imagebwresized)
+    imagebwresizedinvertedscaled = np.asarray(imagebwresizedinvertedscaled)/max_pixel
+    test_sample = np.array(imagebwresizedinvertedscaled).reshape(1,784)
+    test_pred = clf.predict(test_sample)
+    return test_pred[0]
